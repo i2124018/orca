50315399cb4855c0fd27cb0323776b3fda7a3e7a
i2124018 <104130588+i2124018@users.noreply.github.com>
2023-02-27 22:49:42 +0900

    Add files via upload

---
+++ ORCA_HomePage/slick/common.js
@@ -0,0 +1,13 @@
+$('.slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    centerMode: true,
+    centerPadding:"20%",
+    variableWidth: true,
+    autoplaySpeed: 1500,
+    pauseOnFocus: false,
+    pauseOnHover: false,
+    pauseOnDotsHover: false,
+    arrows: false
+});
